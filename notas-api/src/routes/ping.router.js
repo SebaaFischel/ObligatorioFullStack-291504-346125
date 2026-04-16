ef10d37d488e23bf87b96810ca122947024d66b0
SebaaFischel <SF291504@FI365.ORT.EDU.UY>
2026-04-15 21:22:09 -0300

    Eliminar carpeta notas-api que se subió por error

--- notas-api/src/routes/ping.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { ping } from "../controllers/ping.controller.js";
-
-const pingRouter = express.Router();
-
-pingRouter.get('/ping', ping)
-
-export { pingRouter }
