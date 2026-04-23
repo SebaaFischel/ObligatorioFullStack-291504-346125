add3324f83d5fce54492ff34c75a154bf381c823
SebaaFischel <SF291504@FI365.ORT.EDU.UY>
2026-04-23 03:17:30 -0300

    Limpieza final de carpetas innecesarias

--- notas-api/src/routes/notas.admin.router.v1.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { obtenerTodasLasNotas } from "../controllers/notas.admin.controller.js";
-
-const notasAdminRouterV1 = express.Router();
-
-notasAdminRouterV1.get("/notas", obtenerTodasLasNotas)
-
-export { notasAdminRouterV1 }
